b4ce2d127946560add162b7147995ead593c753b
shohel1992 <rndacount@gmail.com>
2024-08-18 08:08:41 +0000

    cc

--- Jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
+++ Jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2024-08-18T07:37:47.0651109Z","EndTimeUtc":"2024-08-18T07:37:49.4085903Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
+{"StartTimeUtc":"2024-08-18T07:40:56.1464423Z","EndTimeUtc":"2024-08-18T07:40:56.1597256Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
